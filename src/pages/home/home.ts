9d8e6b9a4c065a649da112b8c21ea9e970a7b7f5
mediaotg <esty.mediaotg@gmail.com>
2017-11-01 16:55:14 -0400

    wip: Search bar, infinite scroll

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { NavController } from 'ionic-angular';
 import { Drivers } from '../../providers/drivers/drivers';
 import { DriverActivityPage } from '../driverActivity/driverActivity';
 
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -18,6 +21,7 @@ export class HomePage {
   }
 
   getDrivers() {
+    console.log(this.drivers.fetchDrivers());
     this.drivers.fetchDrivers().subscribe(
       data => {
         this.fetchedDrivers = data.json()
@@ -29,6 +33,28 @@ export class HomePage {
 
   driverActivity(driver) {
     this.navCtrl.push(DriverActivityPage, { driver: driver });
+  }
+
+  doInfinite(infiniteScroll){
+    this.drivers.fetchDrivers().subscribe(
+      data => {
+        for(let driver of data.json()){
+          this.fetchedDrivers.push(driver);
+        }
+
+        infiniteScroll.complete();
+      },
+      err => console.error(err),
+      () => console.log('getDrivers completed')
+    );
+  }
+
+  /* Search */
+  onInput(event){
+    console.log('input');
+  }
+  onCancel(event){
+    console.log('cancel');
   }
 
 }
